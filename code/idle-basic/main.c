ee354a997f00ccf07e5c32f49eb740cea7a47f74
dmlaziuk <dm.laziuk@gmail.com>
2019-07-14 20:26:57 +0300

    Run code samples from start to networking

--- code/idle-basic/main.c
+++ code/idle-basic/main.c
@@ -4,21 +4,23 @@
 int64_t counter = 0;
 
 void wait_for_a_while(uv_idle_t* handle) {
-    counter++;
+  counter++;
 
-    if (counter >= 10e6)
-        uv_idle_stop(handle);
+  if (counter >= 10e6)
+    uv_idle_stop(handle);
 }
 
 int main() {
-    uv_idle_t idler;
+  uv_idle_t idler;
+  uv_loop_t *loop = uv_default_loop();
 
-    uv_idle_init(uv_default_loop(), &idler);
-    uv_idle_start(&idler, wait_for_a_while);
+  uv_idle_init(loop, &idler);
+  uv_idle_start(&idler, wait_for_a_while);
 
-    printf("Idling...\n");
-    uv_run(uv_default_loop(), UV_RUN_DEFAULT);
+  printf("Size of uv_idle_t: %zu\n", sizeof(uv_idle_t));
+  printf("Idling...\n");
+  uv_run(loop, UV_RUN_DEFAULT);
 
-    uv_loop_close(uv_default_loop());
-    return 0;
+  uv_loop_close(uv_default_loop());
+  return 0;
 }
